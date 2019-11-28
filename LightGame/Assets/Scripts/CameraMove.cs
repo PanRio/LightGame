d2457caded32ff78708c986976e6184b27d70d06
Roberto <roberto.laczak@gmail.com>
2019-11-28 16:33:52 +0100

    sporo zmian

--- LightGame/Assets/Scripts/CameraMove.cs
+++ LightGame/Assets/Scripts/CameraMove.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class CameraMove : MonoBehaviour
 {
     public Transform toFollow;
     public Vector3 offset = new Vector3(0,0,0);
